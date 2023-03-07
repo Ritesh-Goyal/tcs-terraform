58fa5a431df7d8156e909f8d13a2c0f96445c627
Ritesh G <ritesh.devopstrainer@gmail.com>
2023-03-07 06:34:38 +0000

    adding new example

--- example.tf
+++ example.tf
@@ -1,22 +1,30 @@
-locals {
-  env   = "forum"
-  owner = "Ritesh-DevOps"
-}
-variable "region" {
-  default = "ap-south-1"
-}
-provider "aws" {
-  region = var.region
-}
-resource "aws_instance" "instance01" {
-  ami           = "ami-04db49c0fb2215364"
-  instance_type = "t2.micro"
-  tags = {
-    "Name"        = "web-server"
-    "environment" = local.env
-    "owner"       = local.owner
+terraform {
+  backend "s3" {
+    bucket = "ritesh-aws-infrastructure-tfstate"
+    key    = "devops/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
-output "ip" {
-  value = aws_instance.instance01.public_ip
+
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "instance01" {
+  ami           = "ami-09ba48996007c8b50"
+  instance_type = "t2.micro"
+  key_name      = "ritesh-aws-key"
+  tags = {
+    Name = "terraform-devops"
+  }
+  security_groups = ["${aws_security_group.AWSaccess.name}"]
+}
+resource "aws_security_group" "AWSaccess" {
+  name        = "AWSaccess"
+  description = "SSH access"
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
